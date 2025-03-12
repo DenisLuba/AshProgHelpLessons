def779ec17f1d19cb77e014be444bf0f10fc0ba7
DenisLuba <DenisLUbaPershins@gmail.com>
2025-03-12 10:39:04 +0300

    first commit

---
+++ MenuBar_MAUI/MenuBar_MAUI/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MenuBar_MAUI;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void SaveMenuItem_Clicked(object sender, EventArgs e)
+    {
+        DisplayAlert(title: "Save", message: "You have clicked on Save Menu", cancel: "Ok");
+    }
+}
